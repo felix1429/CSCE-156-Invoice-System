0fb655d731ede07e32cdfbe56ba3d204b70fb0b8
felix1429 <thennig314@gmail.com>
2014-09-25 20:30:44 -0500

    reformat to work on webgrader

--- src/data/files/DataConverter.java
+++ src/data/files/DataConverter.java
@@ -4,18 +4,6 @@ import java.io.*;
 
 public class DataConverter {
 
-    //directory that holds all input files
-    public final File inputFileRelativePath = new File("dataFiles/input");
-
-    //directory that holds all output files
-    public final File outputFileRelativePath = new File("dataFiles/output");
-
-    //list of files in input dir
-    File[] listOfInputFiles = inputFileRelativePath.listFiles();
-
-    File[] listOfOutputFiles = outputFileRelativePath.listFiles();
-
-
     public static void main(String args[]) throws IOException {
         DataConverter dc = new DataConverter();
         dc.iterateOverInputFiles();
@@ -23,32 +11,18 @@ public class DataConverter {
 
     //iterates over files in input dir
     public void iterateOverInputFiles() throws IOException {
-        for(File inputFile : listOfInputFiles) {
-            if(inputFile.getName().equals("Persons.dat")) {
-                PersonDataFile pData = new PersonDataFile(inputFile.getAbsolutePath());
-                writeToFile(pData);
-                for (File inputFile1 : listOfInputFiles) {
-                    if (inputFile1.getName().equals("Customers.dat")) {
-                        CustomerDataFile cData = new CustomerDataFile(inputFile1.getAbsolutePath());
-                        writeToFile(cData);
-                    } else if (inputFile1.getName().equals("Products.dat")) {
-                        ProductDataFile prData = new ProductDataFile(inputFile1.getAbsolutePath());
-                        writeToFile(prData);
-                    }
-                }
-            }
-        }
+        PersonDataFile pData = new PersonDataFile("data/Persons.dat");
+        writeToFile(pData, pData.getName().substring(0, pData.getName().length() - 4));
+        CustomerDataFile cData = new CustomerDataFile("data/Customers.dat");
+        writeToFile(cData, cData.getName().substring(0, cData.getName().length() - 4));
+        ProductDataFile prData = new ProductDataFile("data/Products.dat");
+        writeToFile(prData, prData.getName().substring(0, prData.getName().length() - 4));
     }
 
-    public void writeToFile(DataFile theFile) throws IOException {
-        for(File outputFile : listOfOutputFiles) {
-            String inputName = theFile.getName();
-            String outputName = outputFile.getName();
-            if(inputName.substring(0, inputName.length() - 4).equals(outputName.substring(0, outputName.length() - 5))) {
-                BufferedWriter bw = new BufferedWriter(new FileWriter(outputFile));
-                bw.write(theFile.getOuterJSONObject());
-                bw.close();
-            }
-        }
+    public void writeToFile(DataFile theFile, String fileName) throws IOException {
+        File json = new File("data/" + fileName + ".json");
+        BufferedWriter bw = new BufferedWriter(new FileWriter(json));
+        bw.write(theFile.getOuterJSONObject());
+        bw.close();
     }
 }
